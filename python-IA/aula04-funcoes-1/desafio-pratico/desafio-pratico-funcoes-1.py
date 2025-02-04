71e63584cf508ca7d4973d354bdcd2472266e42e
throdriguesr <throdriguesmg@gmail.com>
2025-02-04 00:16:44 -0300

    Adicionando desafio prático da Aula 04 de python-IA

---
+++ python-IA/aula04-funcoes-1/desafio-pratico/desafio-pratico-funcoes-1.py
@@ -0,0 +1,13 @@
+'''
+DESAFIO PRÁTICO
+
+Calculadora
+
+Crie uma calculadora com opções de soma, multiplicação, subtração, divisão e sair.
+
+(Ela deverá funcionar infinitamente, até que o usuário decida sair da calculadora.)
+
+Utilize funções de rotina para cada operação e funções de unidade lógica para realizar os cálculos.
+
+'''
+
