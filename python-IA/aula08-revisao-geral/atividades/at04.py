7878a6975012317e0c241801944d61f4772e001f
throdriguesr <throdriguesmg@gmail.com>
2025-03-31 22:23:34 -0300

    Adicionando atividade 04 da aula 08 de python-IA

---
+++ python-IA/aula08-revisao-geral/atividades/at04.py
@@ -0,0 +1,12 @@
+'''
+Crie uma função que receba uma lista de strings e retorne uma nova lista contendo apenas as strings palíndromos.
+
+'''
+
+def eh_palindromo(palavra):
+    return palavra == palavra[::-1]
+
+palavras = input("Digite palavras separadas por espaço: ").split()
+palindromos = [p for p in palavras if eh_palindromo(p)]
+
+print("Palavras palíndromas:", palindromos if palindromos else "Nenhuma")
