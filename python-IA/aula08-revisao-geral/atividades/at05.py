9d187e786e3d429c496a832922f286d5f214aa5f
throdriguesr <throdriguesmg@gmail.com>
2025-03-31 22:28:25 -0300

    Adicionando atividade 05 da aula 08 de python-IA

---
+++ python-IA/aula08-revisao-geral/atividades/at05.py
@@ -0,0 +1,20 @@
+'''
+Dado um dicionário que representa as vendas de produtos, encontre o produto mais vendido (ou os produtos mais vendidos, se houver um empate).
+
+'''
+
+vendas = {
+    "maçã": 10,
+    "banana": 25,
+    "laranja": 25,
+    "uva": 15
+}
+
+maior_venda = max(vendas.values())
+
+mais_vendidos = []
+for produto, quantidade in vendas.items():
+    if quantidade == maior_venda:
+        mais_vendidos.append(produto)
+
+print("Produto(s) mais vendido(s):", mais_vendidos)
