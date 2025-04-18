93cc40547330017373cda4527024b0cdaf0153bd
throdriguesr <throdriguesmg@gmail.com>
2025-04-18 00:05:21 -0300

    Adicionando atividade 02 da aula 15 de python-IA

---
+++ python-IA/aula15-SQL-01/atividades/at02.sql
@@ -0,0 +1,9 @@
+'''
+ATIVIDADE PRÁTICA 2
+
+Usando o comando TRUNCATE TABLE, exclua todos os dados da tabela "matriculas" sem excluir a estrutura da tabela.
+
+'''
+
+USE escola;
+TRUNCATE TABLE matriculas;
