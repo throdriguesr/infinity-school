1c471bbe97a7c8111f9c4345d01396718987706f
throdriguesr <throdriguesmg@gmail.com>
2025-04-18 00:10:36 -0300

    Adicionando atividade 03 da aula 15 de python-IA

---
+++ python-IA/aula15-SQL-01/atividades/at03.sql
@@ -0,0 +1,9 @@
+'''
+ATIVIDADE PRÁTICA 3
+
+Usando o comando DROP DATABASE, exclua o banco de dados "escola". 
+Certifique-se de que você tenha feito um backup dos dados, pois essa ação apagará todo o banco de dados.
+
+'''
+
+DROP DATABASE escola;
