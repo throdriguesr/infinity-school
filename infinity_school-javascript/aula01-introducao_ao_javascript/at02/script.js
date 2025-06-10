74fc4b87ba47f445d663cf881f545d88c0051805
throdriguesr <throdriguesmg@gmail.com>
2025-06-09 23:34:55 -0300

    feat: adiciona atividade 02 da aula 01 de JavaScript

---
+++ infinity_school-javascript/aula01-introducao_ao_javascript/at02/script.js
@@ -0,0 +1,8 @@
+// Declarando uma variável com var
+var mensagem;
+
+// Atribuindo um valor à variável
+mensagem = "Essa é a atividade 02";
+
+// Exibindo o valor da variável usando alert
+alert(mensagem);
