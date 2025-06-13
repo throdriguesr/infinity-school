1e9126b8516ef8e13dc7c1f2f815e2ae627ce871
throdriguesr <throdriguesmg@gmail.com>
2025-06-13 15:14:53 -0300

    feat: adiciona atividade 06 da aula 01 de JavaScript

---
+++ infinity_school-javascript/aula01-introducao_ao_javascript/at06/script.js
@@ -0,0 +1,29 @@
+// Tipo texto (string)
+let nome = "Thiago";
+
+// Tipo número (number)
+let idade = 30;
+
+// Tipo verdadeiro ou falso (boolean)
+let estaEstudando = true;
+
+// Mostrar as variáveis no console
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Está estudando?", estaEstudando);
+
+// Contas com dois números
+let numero1 = 10;
+let numero2 = 5;
+
+// Operações
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+// Mostrar os resultados no console
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
