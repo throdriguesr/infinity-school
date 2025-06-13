596988b63a6be0d3d145e81e1a3ac9c98c0daeda
throdriguesr <throdriguesmg@gmail.com>
2025-06-13 14:58:06 -0300

    feat: adiciona atividade 05 da aula 01 de JavaScript

---
+++ infinity_school-javascript/aula01-introducao_ao_javascript/at05/script.js
@@ -0,0 +1,15 @@
+let numero1 = prompt("Digite o primeiro número:");
+let numero2 = prompt("Digite o segundo número:");
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+alert("Soma: " + soma);
+alert("Subtração: " + subtracao);
+alert("Multiplicação: " + multiplicacao);
+alert("Divisão: " + divisao);
