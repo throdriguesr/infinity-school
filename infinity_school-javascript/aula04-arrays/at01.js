40457f5609fb3af1bae3f010d49bef7e0a42a4ae
throdriguesr <throdriguesmg@gmail.com>
2025-07-25 01:15:54 -0300

    Adicionando atividade 01 da aula 04 de JavaScript

---
+++ infinity_school-javascript/aula04-arrays/at01.js
@@ -0,0 +1,11 @@
+// Atividade 01
+// Primeiro, crie um array chamado cores contendo os seguintes elementos:
+// Vermelho, Verde, Azul, Amarelo.
+// Em seguida, acesse e exiba no console o elemento na posição 3.
+//
+// Objetivo:
+// Praticar o uso de índices para acessar elementos específicos em um array.
+
+let cores = ["Vermelho", "Verde", "Azul", "Amarelo"];
+
+console.log(cores[3]);
