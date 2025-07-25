b00eb8e60d75b3800fdbfb38e26bc3ab2e3bbc75
throdriguesr <throdriguesmg@gmail.com>
2025-07-24 23:22:34 -0300

    Adicionando atividade 01 da aula 03 de JavaScript

---
+++ infinity_school-javascript/aula03-lacos_de_repeticao/at01-while.js
@@ -0,0 +1,12 @@
+// Atividade 01
+// Crie um código que utilize um loop while para contar de 1 a 10 e exibir cada número no console.
+
+// Objetivo:
+// Aprender a utilizar a estrutura de repetição while para executar um bloco de código repetidamente enquanto uma condição é verdadeira.
+
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
