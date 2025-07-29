73e10a22b2c0714e6462d2cb87135b0ca9687653
throdriguesr <throdriguesmg@gmail.com>
2025-07-28 22:40:13 -0300

    Adicionando atividade 02 da aula 05 de JavaScript

---
+++ infinity_school-javascript/aula05-revisao/at02.js
@@ -0,0 +1,38 @@
+// [Atividade 02]
+// Crie um arquivo JavaScript e siga os passos abaixo para praticar
+// a manipulação de variáveis e a realização de operações básicas.
+//
+// Objetivo:
+// Realizar operações básicas de soma, subtração, multiplicação e divisão.
+//
+// Declare quatro variáveis:
+// - Use let para declarar duas variáveis, num1 e num2, e atribua a elas dois números à sua escolha.
+// - Use const para declarar duas variáveis, nome e cidade, e atribua uma string para cada uma delas
+//   (seu nome e a cidade onde você mora).
+//
+// 1. Crie novas variáveis para armazenar o resultado das seguintes operações entre num1 e num2:
+//    - Soma (soma)
+//    - Subtração (subtracao)
+//    - Multiplicação (multiplicacao)
+//    - Divisão (divisao)
+//
+// 2. Use console.log() para exibir os resultados das operações e as informações das constantes nome e cidade.
+
+let num1 = 10;
+let num2 = 5;
+
+const nome = "Thiago";
+const cidade = "Belo Horizonte";
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log("Nome:", nome);
+console.log("Cidade:", cidade);
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
