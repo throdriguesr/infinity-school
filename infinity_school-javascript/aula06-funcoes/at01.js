db83edd697a54153b9a2252009304f96ef4e6280
throdriguesr <throdriguesmg@gmail.com>
2025-07-30 15:59:58 -0300

    Adicionando atividade 01 da aula 06 de JavaScript

---
+++ infinity_school-javascript/aula06-funcoes/at01.js
@@ -0,0 +1,15 @@
+// Atividade 01:
+// 1. Defina a função exibirMensagem.
+// 2. Dentro da função, utilize console.log para exibir uma mensagem fixa, como "Bem-vindo ao JavaScript!".
+// 3. Chame a função e observe o resultado no console.
+
+// Objetivo:
+// Praticar a criação de funções em JavaScript sem a necessidade de utilizar parâmetros,
+// focando na execução de ações dentro da função.
+
+function exibirMensagem() {
+  console.log("Bem-vindo ao JavaScript!");
+}
+
+// Chamando a função
+exibirMensagem();
