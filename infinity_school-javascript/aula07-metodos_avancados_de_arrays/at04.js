7182612dae271a067b0eab9cd202248c36b3f366
throdriguesr <throdriguesmg@gmail.com>
2025-08-25 22:37:28 -0300

    Adicionando atividade 04 da aula 07 de JavaScript

---
+++ infinity_school-javascript/aula07-metodos_avancados_de_arrays/at04.js
@@ -0,0 +1,18 @@
+/*
+Atividade 04
+Crie um script que utilize o método reduce() para somar todos os
+números de um array. O script deve calcular a soma total e exibir o
+resultado no console.
+
+Objetivo:
+Praticar o uso do método reduce() para acumular valores em um
+array, resultando em um único valor.
+*/
+
+const numeros = [1, 2, 3, 4, 5];
+
+const somaTotal = numeros.reduce(function(acumulador, numero) {
+    return acumulador + numero;
+}, 0);
+
+console.log("A soma total é:", somaTotal);
