73af853226ce0f81603045c0aa289d3dd938ea12
throdriguesr <throdriguesmg@gmail.com>
2025-08-27 23:22:17 -0300

    Adicionando atividade 02 da aula 08 de JavaScript

---
+++ infinity_school-javascript/aula08-objetos_literais/at02.js
@@ -0,0 +1,26 @@
+/*
+Atividade 02
+Crie um objeto chamado pessoa com as
+propriedades nome, idade, e profissao.
+
+Em seguida:
+Altere a propriedade profissao para um novo valor.
+Adicione uma nova propriedade chamada cidade ao objeto.
+Exiba no console o objeto completo após as modificações.
+
+Objetivo:
+Praticar a manipulação de objetos, alterando propriedades existentes,
+adicionando novas propriedades, e exibindo o objeto atualizado.
+*/
+
+let pessoa = {
+    nome: "Thiago",
+    idade: 30,
+    profissao: "Engenheiro de Software"
+};
+
+pessoa.profissao = "Engenheiro de Dados";
+
+pessoa.cidade = "Belo Horizonte";
+
+console.log(pessoa);
