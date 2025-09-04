a08295bf7b229cf7034e7d2def3b4d4454153bed
throdriguesr <throdriguesmg@gmail.com>
2025-09-03 23:34:50 -0300

    Adiciona atividade 02 da aula 09 de JavaScript

---
+++ infinity_school-javascript/aula09-revisao_ll/at02.js
@@ -0,0 +1,20 @@
+/* 
+Atividade 02
+Crie uma função chamada saudarUsuario que receba um parâmetro chamado nome.
+
+A função deve exibir no console uma mensagem de saudação personalizada, como "Olá, [nome]!".
+
+Chame a função três vezes, passando nomes diferentes como argumento.
+
+Objetivo:
+Praticar a criação de funções que recebem parâmetros.
+*/
+
+function saudarUsuario(nome) {
+  console.log("Olá, " + nome + "!");
+}
+
+// Chamando a função três vezes com nomes diferentes
+saudarUsuario("Thiago");
+saudarUsuario("Maria");
+saudarUsuario("João");
