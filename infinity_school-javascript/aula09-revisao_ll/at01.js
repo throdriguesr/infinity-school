29962459639830a9f4bb729186abf97dac002c9e
throdriguesr <throdriguesmg@gmail.com>
2025-09-03 23:33:27 -0300

    Adiciona atividade 01 da aula 09 de JavaScript

---
+++ infinity_school-javascript/aula09-revisao_ll/at01.js
@@ -0,0 +1,17 @@
+/*
+Atividade 01
+Crie uma função chamada exibirBoasVindas que, quando chamada, exiba no console a mensagem: "Bem-vindo ao curso de JavaScript!".
+
+Chame a função duas vezes para verificar que ela funciona corretamente.
+
+Objetivo:
+Praticar a criação e chamada de funções sem parâmetros.
+*/
+
+function exibirBoasVindas() {
+  console.log("Bem-vindo ao curso de JavaScript!");
+}
+
+// Chamando a função duas vezes
+exibirBoasVindas();
+exibirBoasVindas();
