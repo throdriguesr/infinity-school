7eeab29e617aa186bb6d7acdabab142c073a4420
throdriguesr <throdriguesmg@gmail.com>
2025-09-06 22:31:20 -0300

    Adiciona atividade 01 da aula 11 de JavaScript

---
+++ infinity_school-javascript/aula11-dom_ll/at01/script.js
@@ -0,0 +1,7 @@
+// Pega o botão do HTML
+const botao = document.getElementById("meuBotao");
+
+// Adiciona evento de clique
+botao.addEventListener("click", () => {
+  alert("Botão clicado!"); // Mostra alerta quando clicado
+});
